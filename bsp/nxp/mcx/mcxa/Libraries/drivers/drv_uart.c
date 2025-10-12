ad1dbd9cb6f23d48346e1b3834466a8f542fdb02
Yucai Liu <yucai.liu@sunseekerpower.cn>
2025-10-12 21:03:23 +0800

    [bsp/nxp/mcx/mcxa] Fix UART clock configuration type mismatch
    Fixed implicit type conversion warning in UART driver by correctly
    separating clock_name_t and clock_attach_id_t enum types.
    
    Changes:
    - Use kCLOCK_Fro12M (clock_name_t) for clock_src field
    - Keep kFRO12M_to_LPUARTx (clock_attach_id_t) for clock_attach_id field
    - Remove unnecessary CPU model conditional compilation
    - Fix mcx_getc() to use correct status flag kLPUART_RxDataRegFullFlag
    
    This resolves the compiler warning:
    "implicit conversion from 'int' to 'clock_name_t' changes value from 16777216 to 0"
    
    Signed-off-by: Yucai Liu 1486344514@qq.com

--- bsp/nxp/mcx/mcxa/Libraries/drivers/drv_uart.c
+++ bsp/nxp/mcx/mcxa/Libraries/drivers/drv_uart.c
@@ -64,11 +64,7 @@ static const struct mcx_uart uarts[] =
         &serial0,
         LPUART0,
         LPUART0_IRQn,
-#if (defined(CPU_MCXA346VLH) || defined(CPU_MCXA346VLL) || defined(CPU_MCXA346VLQ) || defined(CPU_MCXA346VPN))
-        kFRO_LF_DIV_to_LPUART0,
-#else
-        kFRO12M_to_LPUART0,
-#endif
+        kCLOCK_Fro12M,
         kFRO12M_to_LPUART0,
         kCLOCK_GateLPUART0,
         kCLOCK_DivLPUART0,
@@ -80,11 +76,7 @@ static const struct mcx_uart uarts[] =
         &serial1,
         LPUART1,
         LPUART1_IRQn,
-#if (defined(CPU_MCXA346VLH) || defined(CPU_MCXA346VLL) || defined(CPU_MCXA346VLQ) || defined(CPU_MCXA346VPN))
-        kFRO_LF_DIV_to_LPUART1,
-#else
-        kFRO12M_to_LPUART1,
-#endif
+        kCLOCK_Fro12M,
         kFRO12M_to_LPUART1,
         kCLOCK_GateLPUART1,
         kCLOCK_DivLPUART1,
@@ -97,11 +89,7 @@ static const struct mcx_uart uarts[] =
         LPUART2,
         LPUART2_IRQn,
         kCLOCK_Fro12M,
-#if (defined(CPU_MCXA346VLH) || defined(CPU_MCXA346VLL) || defined(CPU_MCXA346VLQ) || defined(CPU_MCXA346VPN))
-        kFRO_LF_DIV_to_LPUART2,
-#else
         kFRO12M_to_LPUART2,
-#endif
         kCLOCK_GateLPUART2,
         kCLOCK_DivLPUART2,
         "uart2",
@@ -182,7 +170,7 @@ static int mcx_getc(struct rt_serial_device *serial)
 {
     struct mcx_uart *uart = (struct mcx_uart *)serial->parent.user_data;
 
-    if (kLPUART_RxDataRegFullInterruptEnable & LPUART_GetStatusFlags(uart->uart_base))
+    if (kLPUART_RxDataRegFullFlag & LPUART_GetStatusFlags(uart->uart_base))
     {
         return LPUART_ReadByte(uart->uart_base);
     }
